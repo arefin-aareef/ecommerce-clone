ae5463d1ed476553793f2c53f2e8c22e447e2528
Arefin Aareef <arefin.aareef@gmail.com>
2024-01-01 23:18:44 +0600

    muliti layer drawer added

--- src/components/navbar/NavbarTopLeft.tsx
+++ src/components/navbar/NavbarTopLeft.tsx
@@ -1,97 +1,202 @@
+// import {
+// 	Drawer,
+// 	DrawerBody,
+// 	DrawerCloseButton,
+// 	DrawerContent,
+// 	DrawerOverlay,
+// 	Flex,
+// 	IconButton,
+// 	Text,
+// 	useDisclosure,
+// } from '@chakra-ui/react';
+// import React, { FC, useRef } from 'react';
+// import { CiLocationOn, CiSearch } from 'react-icons/ci';
+// import { FaBars } from 'react-icons/fa';
+// import { MdOutlineKeyboardArrowRight } from 'react-icons/md';
+// import { navCategories } from '../data/NavbarData';
+
+// type NavbarTopLeftProps = {};
+
+// const NavbarTopLeft: FC<NavbarTopLeftProps> = ({}) => {
+// 	const { isOpen, onOpen, onClose } = useDisclosure();
+// 	const btnRef = useRef<HTMLButtonElement>(null);
+
+// 	return (
+// 		<Flex alignItems='center'>
+// 			{/* For Mobile */}
+// 			<Flex display={{ base: 'flex', xl: 'none' }} gap={4} alignItems='center'>
+// 				<IconButton
+// 					ref={btnRef}
+// 					bg='transparent'
+// 					onClick={onOpen}
+// 					aria-label='menu'
+// 					icon={<FaBars size={24} />}
+// 					_hover={{ bg: 'transparent' }}
+// 				/>
+
+// 				<Drawer
+// 					isOpen={isOpen}
+// 					placement='left'
+// 					onClose={onClose}
+// 					finalFocusRef={btnRef}
+// 				>
+// 					<DrawerOverlay />
+
+// 					<DrawerContent position='relative'>
+// 						<DrawerCloseButton
+// 							position='absolute'
+// 							color='white'
+// 							rounded='unset'
+// 							right={-8}
+// 							top={0}
+// 							bg='red'
+// 						/>
+
+// 						<DrawerBody>
+// 							{navCategories.map(item => (
+// 								<Flex
+// 									as='button'
+// 									w='full'
+// 									borderBottom='1px solid grey'
+// 									_hover={{ bg: 'sidebarHoverColor' }}
+// 									alignItems='center'
+// 									justifyContent='space-between'
+// 								>
+// 									<Text py={2} fontWeight={600}>
+// 										{item.name}
+// 									</Text>
+// 									<MdOutlineKeyboardArrowRight />
+// 								</Flex>
+// 							))}
+// 						</DrawerBody>
+// 					</DrawerContent>
+// 				</Drawer>
+
+// 				<Flex cursor='pointer'>
+// 					<CiSearch size={24} />
+// 				</Flex>
+// 			</Flex>
+
+// 			{/* For Desktop */}
+// 			<Flex
+// 				display={{ base: 'none', xl: 'flex' }}
+// 				gap={1.5}
+// 				alignItems='center'
+// 			>
+// 				<Flex cursor='pointer' gap={1.5}>
+// 					<CiLocationOn size={24} />
+// 					<Text>FIND A STORE</Text>
+// 				</Flex>
+// 				<Text>|</Text>
+// 				<Text cursor='pointer'>CUSTOMER CARE: 09666200300</Text>
+// 			</Flex>
+// 		</Flex>
+// 	);
+// };
+
+// export default NavbarTopLeft;
+
+
+
+// NavbarTop.tsx
 import {
-	Drawer,
-	DrawerBody,
-	DrawerCloseButton,
-	DrawerContent,
-	DrawerOverlay,
-	Flex,
-	IconButton,
-	Text,
-	useDisclosure,
+  Drawer,
+  DrawerBody,
+  DrawerCloseButton,
+  DrawerContent,
+  DrawerOverlay,
+  Flex,
+  IconButton,
+  Text,
+  useDisclosure,
 } from '@chakra-ui/react';
-import React, { FC, useRef } from 'react';
-import { CiLocationOn, CiSearch } from 'react-icons/ci';
+import React, { FC, useRef, useState } from 'react';
 import { FaBars } from 'react-icons/fa';
 import { MdOutlineKeyboardArrowRight } from 'react-icons/md';
+import { CiSearch, CiLocationOn } from 'react-icons/ci';
+import SubcategoryDrawer from './SubcategoryDrawer'; // Import your SubcategoryDrawer component
 import { navCategories } from '../data/NavbarData';
 
-type NavbarTopLeftProps = {};
+type NavbarTopProps = {};
 
-const NavbarTopLeft: FC<NavbarTopLeftProps> = ({}) => {
-	const { isOpen, onOpen, onClose } = useDisclosure();
-	const btnRef = useRef<HTMLButtonElement>(null);
+const NavbarTop: FC<NavbarTopProps> = () => {
+  const { isOpen, onOpen, onClose } = useDisclosure();
+  const btnRef = useRef<HTMLButtonElement>(null);
+  const [selectedCategory, setSelectedCategory] = useState<string | null>(null);
 
-	return (
-		<Flex alignItems='center'>
-			{/* For Mobile */}
-			<Flex display={{ base: 'flex', xl: 'none' }} gap={4} alignItems='center'>
-				<IconButton
-					ref={btnRef}
-					bg='transparent'
-					onClick={onOpen}
-					aria-label='menu'
-					icon={<FaBars size={24} />}
-					_hover={{ bg: 'transparent' }}
-				/>
+  const openSubcategoryDrawer = (category: string) => {
+    setSelectedCategory(category);
+    onOpen();
+  };
 
-				<Drawer
-					isOpen={isOpen}
-					placement='left'
-					onClose={onClose}
-					finalFocusRef={btnRef}
-				>
-					<DrawerOverlay />
+  return (
+    <Flex alignItems='center'>
+      {/* For Mobile */}
+      <Flex display={{ base: 'flex', xl: 'none' }} gap={4} alignItems='center'>
+        <IconButton
+          ref={btnRef}
+          bg='transparent'
+          onClick={onOpen}
+          aria-label='menu'
+          icon={<FaBars size={24} />}
+          _hover={{ bg: 'transparent' }}
+        />
 
-					<DrawerContent position='relative'>
-						<DrawerCloseButton
-							position='absolute'
-							color='white'
-							rounded='unset'
-							right={-8}
-							top={0}
-							bg='red'
-						/>
+        {/* Main Categories Drawer */}
+        <Drawer isOpen={isOpen} onClose={onClose} size='xs' placement='left' finalFocusRef={btnRef}>
+          <DrawerOverlay />
+          <DrawerContent>
+            <DrawerCloseButton />
+            <DrawerBody>
+              <Flex direction='column'>
+                {navCategories.map((item) => (
+                  <Flex
+                    key={item.id}
+                    as='button'
+                    w='full'
+                    borderBottom='1px solid grey'
+                    _hover={{ bg: 'sidebarHoverColor' }}
+                    alignItems='center'
+                    justifyContent='space-between'
+                    onClick={() => openSubcategoryDrawer(item.name)}
+                  >
+                    <Text py={2} fontWeight={600}>
+                      {item.name}
+                    </Text>
+                    <MdOutlineKeyboardArrowRight />
+                  </Flex>
+                ))}
+              </Flex>
+            </DrawerBody>
+          </DrawerContent>
+        </Drawer>
 
-						<DrawerBody>
-							{navCategories.map(item => (
-								<Flex
-									as='button'
-									w='full'
-									borderBottom='1px solid grey'
-									_hover={{ bg: 'sidebarHoverColor' }}
-									alignItems='center'
-									justifyContent='space-between'
-								>
-									<Text py={2} fontWeight={600}>
-										{item.name}
-									</Text>
-									<MdOutlineKeyboardArrowRight />
-								</Flex>
-							))}
-						</DrawerBody>
-					</DrawerContent>
-				</Drawer>
+        <Flex cursor='pointer'>
+          <CiSearch size={24} />
+        </Flex>
+      </Flex>
 
-				<Flex cursor='pointer'>
-					<CiSearch size={24} />
-				</Flex>
-			</Flex>
+      {/* For Desktop */}
+      <Flex display={{ base: 'none', xl: 'flex' }} gap={1.5} alignItems='center'>
+        <Flex cursor='pointer' gap={1.5}>
+          <CiLocationOn size={24} />
+          <Text>FIND A STORE</Text>
+        </Flex>
+        <Text>|</Text>
+        <Text cursor='pointer'>CUSTOMER CARE: 09666200300</Text>
+      </Flex>
 
-			{/* For Desktop */}
-			<Flex
-				display={{ base: 'none', xl: 'flex' }}
-				gap={1.5}
-				alignItems='center'
-			>
-				<Flex cursor='pointer' gap={1.5}>
-					<CiLocationOn size={24} />
-					<Text>FIND A STORE</Text>
-				</Flex>
-				<Text>|</Text>
-				<Text cursor='pointer'>CUSTOMER CARE: 09666200300</Text>
-			</Flex>
-		</Flex>
-	);
+      {/* Subcategories Drawer */}
+      {selectedCategory && (
+        <SubcategoryDrawer
+          isOpen={isOpen}
+          onClose={onClose}
+          title={selectedCategory}
+          subcategories={navCategories.find((item) => item.name === selectedCategory)?.categories || [] }
+        />
+      )}
+    </Flex>
+  );
 };
 
-export default NavbarTopLeft;
+export default NavbarTop;
---
+++ src/components/navbar/SubcategoryDrawer.tsx
@@ -0,0 +1,56 @@
+// SubcategoryDrawer.tsx
+import {
+	Drawer,
+	DrawerBody,
+	DrawerCloseButton,
+	DrawerContent,
+	DrawerOverlay,
+	Flex,
+	Text,
+} from '@chakra-ui/react';
+import React, { FC } from 'react';
+
+type SubcategoryDrawerProps = {
+	isOpen: boolean;
+	onClose: () => void;
+	title: string | null;
+	subcategories: {
+		title: string;
+		subCategory?: { subCategoryName: string; href: string }[];
+	}[];
+};
+
+const SubcategoryDrawer: FC<SubcategoryDrawerProps> = ({
+	isOpen,
+	onClose,
+	title,
+	subcategories,
+}) => {
+	return (
+		<Drawer isOpen={isOpen} placement='left' onClose={onClose} size='xs'>
+			<DrawerOverlay />
+			<DrawerContent>
+				<DrawerCloseButton />
+				<DrawerBody>
+					<Flex direction='column'>
+						<Text fontSize='lg' fontWeight='bold' mb={4}>
+							{title}
+						</Text>
+						{subcategories.map(subcategory => (
+							<Flex key={subcategory.title} direction='column'>
+								<Text fontWeight='bold'>{subcategory.title}</Text>
+								{(subcategory.subCategory ?? []).map(subItem => (
+									<Flex key={subItem.subCategoryName} alignItems='center'>
+										<Text>{subItem.subCategoryName}</Text>
+									</Flex>
+								))}
+							</Flex>
+						))}
+					</Flex>
+				</DrawerBody>
+			</DrawerContent>
+		</Drawer>
+	);
+};
+
+export default SubcategoryDrawer;
